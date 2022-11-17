fdc57acc9844ceb9cb19d8219da49b67305e5abc
freedobby77 <117061022+freedobby77@users.noreply.github.com>
2022-11-17 11:53:10 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,3 +1,9 @@
 function solution(slice, n) {
-    return Math.ceil(n / slice);
+    var answer = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % slice === 0) {
+            answer ++
+        }
+    }
+    return answer;
 }
